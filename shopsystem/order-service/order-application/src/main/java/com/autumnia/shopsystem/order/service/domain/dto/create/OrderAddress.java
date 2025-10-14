4ffa326d609d7d9ced5a32c06a0325e4578eed20
MZC01-KKE\MZC01-KKE <kke@mz.co.kr>
2025-10-14 13:08:46 +0900

    25-10-14 13:00  updated

---
+++ shopsystem/order-service/order-application/src/main/java/com/autumnia/shopsystem/order/service/domain/dto/create/OrderAddress.java
@@ -0,0 +1,11 @@
+package com.autumnia.shopsystem.order.service.domain.dto.create;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class OrderAddress {
+}
