9d136dc6273043d973bcc708db5ad92cbbe2b943
MZC01-KKE\MZC01-KKE <kke@mz.co.kr>
2025-10-28 14:44:14 +0900

    25-10-28 14:44 updated

---
+++ shopsystem/common/common-application/src/main/java/com/autumnia/shopsystem/application/handdler/ErrorDto.java
@@ -0,0 +1,13 @@
+package com.autumnia.shopsystem.application.handdler;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ErrorDto {
+    private final String code;
+    private final String message;
+}
---
+++ shopsystem/common/common-application/src/main/java/com/autumnia/shopsystem/application/handdler/GlobalExceptionHandler.java
@@ -0,0 +1,62 @@
+package com.autumnia.shopsystem.application.handdler;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.ValidationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.stream.Collectors;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value={Exception.class})
+    public ErrorDto handleException(Exception exception) {
+        log.error( exception.getMessage(), exception);
+
+        return ErrorDto.builder()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                .message(exception.getMessage())
+                .build();
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value={Exception.class})
+    public ErrorDto handleException(ValidationException validationException) {
+        ErrorDto errorDto;
+        if ( validationException instanceof ConstraintViolationException ) {
+            String violations = extractViolationsFromException((ConstraintViolationException) validationException);
+            log.error(violations, validationException);
+            errorDto = ErrorDto.builder()
+                    .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                    .message(violations)
+                    .build();
+        }
+        else {
+            String exceptionMessage = validationException.getMessage();
+            log.error(exceptionMessage, validationException);
+            errorDto = ErrorDto.builder()
+                    .code(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                    .message(exceptionMessage)
+                    .build();
+        }
+
+        return errorDto;
+    }
+
+    private String extractViolationsFromException(ConstraintViolationException validationException) {
+        return validationException.getConstraintViolations()
+                .stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.joining("--"));
+    }
+}
